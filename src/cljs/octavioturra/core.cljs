cb7bc116af3d1284e0a8cf44f9b3c2eb7830bd02
Octavio Turra <octavio.turra@gmail.com>
2017-02-15 02:18:04 -0200

    :sparkles: first-functional site

--- src/cljs/octavioturra/core.cljs
+++ src/cljs/octavioturra/core.cljs
@@ -1,8 +1,6 @@
 (ns octavioturra.core
     (:require [reagent.core :as reagent :refer [atom]]
               [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]
               [cljs-http.client :as http]
               [cljs.core.async :refer [put! chan <! >! timeout close!]])
     (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
@@ -22,107 +20,125 @@
   :messages []
   :flow {}
   :step {}
+  :events {}
 }))
 
 ;; -------------------------
-;; Logic
+;; Broker
 
-;; processar mensagem
-(defn proccess-message! [step]
-  (let [text (.-text step)
-        owner (.-owner step)] 
-          (swap! state update-in [:messages] conj {:text text :owner owner :created-at (now)})))
+(defn emit-gtm [event detail] 
+  (if js/window.dataLayer 
+    (js/window.dataLayer.push 
+      (js/Object.assign 
+        (clj->js {:event event}) 
+        (clj->js detail)))))
 
-;; processar variavel
-(defn proccess-variable! [step]
-  (let [variable (.-variable step)
-        value (.-value step)] 
-          (swap! state update-in [:value] conj {:variable variable :value value})))
+(defn dispatch [event detail] 
+  (swap! state update-in [:events] conj {:event event :detail detail})
+  (emit-gtm event detail)
+  (log ["dispatching" event detail])
+  (js/window.dispatchEvent 
+    (js/CustomEvent. event (clj->js {:detail detail}))))
 
-(defn handle-choice [variable value] #(proccess-variable! {:variable variable :value value}))
+(defn listen [event listener] 
+  (js/window.addEventListener event 
+    (fn [ev] 
+      (let [detail (js->clj (.-detail ev))]
+      (log ["receiving" event detail])
+      (listener detail)))))
 
-;; widget-multiple-choice
-(defn multiple-choice [action]
-  (let [answers (.-answers action)
-        variable (.-variable action)] 
-          [:form.wideget-multiple-choice
-            (map #([:button {:onClick (handle-choice variable %1)} %1]) answers)]))
-
-;; renderizar widget
-(def widgets {
-  :multiple-choice multiple-choice
-  :end (fn [_] [:h1 "obrigado"])
-})
-
-(defn render-widget [step] 
-  (log step) 
-  ((get widgets (.-widget step)) step))
-
-(defn proccess-step [{step :step flow :flow :as state}]
-  (let [next (.-next step)
-        widget (.-widget step)]
-          (proccess-message! step)
-          (cond 
-            next (proccess-step (aget flow next))
-            widget (render-widget step)
-            :else (render-widget {:widget :end}))))
-
-;; -------------------------
-;; Components
-
-(defn button [label on-press] (fn [] [:button {:on-click on-press} label]))
-
-(defn avatar [owner] [:img.avatar {:src owner}])
-
-(defn baloon [from message] (fn [] [:div.balloon [avatar from] [:div.content message]]))
-
-(defn deck [messages] (let [state @state] 
-                        (fn []
-                          (if (map (fn [{from :from text :text}] [baloon from text]) messages)
-                              [:div "calma um pouco"])
-                              (proccess-step state))))
-
-;; -------------------------
-;; Views
-
-(defn home-page []
+(defn proccess-step [step]
   (let [
-    {
-      messages :messages
-      current-step :current-step
-    } state
-  ] (fn [] [:div [:h2 "Olá, "]
-      [deck messages]])))
+    next (get step "next")
+    text (get step "text")
+    owner (get step "owner")
+  ] 
+    (log ["step" step])
+    (when text (dispatch "add-text" {"text" text "owner" owner}))
+    (when next (dispatch "set-step" {"step" next}))))
 
-(defn current-page []
-  [:div [(session/get :current-page)]])
+(defn set-step [step-name] 
+    (let [flow (:flow @state)
+          step (get flow step-name)]
+      (swap! state assoc-in [:step] step)
+      (log ["set-step" step-name flow step @state])
+      (proccess-step step)))
+
+(defn add-text [message]
+    (swap! state update-in [:messages] conj message))
+
+(defn listen-set-step []
+  (listen "set-step" 
+    (fn [detail] 
+      (set-step 
+        (get detail "step")))))
+
+(defn listen-add-text []
+  (listen "add-text"
+    (fn [detail]
+      (let [text (get detail "text")
+            owner (get detail "owner")] 
+        (add-text {:text text :owner owner})))))
+
+(defn listen-answered []
+  (listen "answered"
+    (fn [detail]
+      (let [answer (get detail "answer")
+            text (get answer "text")
+            next (get answer "next")
+            owner "me"]
+              (log ["answered" text next])
+              (dispatch "add-text" {"text" text "owner" owner})
+              (dispatch "set-step" {"step" next})))))
+
+(defn listen-all []
+  (listen-add-text)
+  (listen-set-step)
+  (listen-answered))
+
+(defn button-handler [answer] 
+  (fn [] 
+    (dispatch "answered" {"answer" answer})))
+
+(defn footer [answers] 
+    [:footer 
+      (map-indexed 
+        (fn [i answer] 
+          [:button 
+            {:key i :on-click (button-handler answer)} (get answer "text")]) 
+              answers)])
+
+(defn chat [messages]
+    [:article
+      (map-indexed (fn [i message] [:div {:key i} [:small (:owner message)] [:br] (:text message)]) messages)])
 
 ;; -------------------------
-;; Routes
+;; Main Component
 
-(secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
+(defn main [] (fn [] 
+                (let [answers (get (:step @state) "answers")
+                      messages (:messages @state)]
+                        (log ["main" answers messages @state])
+                        [:main
+                          [:header "Cabeçalho"]
+                          (when messages [chat messages])
+                          (when answers [footer answers])])))
 
 ;; -------------------------
-;; Initialize app
+;; Mounting
 
 (defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
+  (reagent/render [main] (.getElementById js/document "app")))
 
 (defn bootstrap! [flow]
-      (swap! state conj {:flow flow :step (.-start flow)})
-      (accountant/configure-navigation!
-        {:nav-handler
-        (fn [path]
-          (secretary/dispatch! path))
-        :path-exists?
-        (fn [path]
-          (secretary/locate-route path))})
-      (accountant/dispatch-current!)
+      (swap! state conj {:flow flow})
+      (listen-all)
+      (dispatch "set-step" {"step" "start"})
       (mount-root))
 
 (defn init! [] 
-  (js/window.addEventListener "app-loaded" (fn [ev] 
-    (let [flow (.-flow (.-detail (js->clj ev)))]
-      (bootstrap! flow))))
-  (js/window.dataLayer.push (clj->js {:event "app-loaded"})))
+  (listen "app-loaded" 
+    (fn [detail] 
+      (let [flow (get detail "flow")]
+        (bootstrap! flow))))
+  (emit-gtm "app-loaded" {}))
